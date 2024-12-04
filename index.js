e85d1a5258a7cd985076c48ad14a6cde34e8d5dd
Daryl Magera <Mageradaryl12@gmail.com>
2024-12-04 07:07:30 -0800

    made changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const logo = document.querySelectorAll("#logo path");
 let slides = document.querySelector("#slides");
 let imgslides = document.querySelectorAll(".slide");
+let images = document.querySelectorAll(".slide img");
 let timeout = 4000;
 let current = -1;
 
 let slider = setTimeout(fadeNextSlide, timeout);
 
-for(let i = 0; i<logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
+// for(let i = 0; i<logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }
 
 function fadeNextSlide() {
   for (let i = 0; i < imgslides.length; i++) {
@@ -24,15 +25,15 @@ function fadeNextSlide() {
   slider = setTimeout(fadeNextSlide, timeout);
 }
 
-function fadePrevSlide() {
-  for (let i = 0; i < imgslides.length; i++) {
-    imgslides[i].style.opacity = 0;
-  }
-  if (current == 0) {
-    current = imgslides.length - 1;
-  } else {
-    current--;
-  }
-  imgslides[current].style.opacity = 1;
-  slider = setTimeout(fadeNextSlide, timeout);
-}
+// function fadePrevSlide() {
+//   for (let i = 0; i < imgslides.length; i++) {
+//     imgslides[i].style.opacity = 0;
+//   }
+//   if (current == 0) {
+//     current = imgslides.length - 1;
+//   } else {
+//     current--;
+//   }
+//   imgslides[current].style.opacity = 1;
+//   slider = setTimeout(fadeNextSlide, timeout);
+// }
